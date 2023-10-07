410361d6697f29e27e09378f9a4e2dc22ba048d6
Sahil Gupta <sahilofficial17@gmail.com>
2023-10-07 13:31:25 -0700

    Construction

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import { Button } from "antd"
 
 export default function Home() {
   return (
-    <div>
-      <Link href="/dashboard">Dashboard</Link>
+    <div className='text-center justify-center align-center'>
+      <div className='text-9xl text-bold text-slate-800' >HomePage Under construction⚠️ . <br />Please visit our dashboard 🥲</div>
+      <Button href="/dashboard" size='large' type="primary">Dashboard</Button>
     </div>
   )
 }
