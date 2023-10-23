c42741c6438ffdf63aa33463bd4234bd2830eb30
Puranjai Garg <puranjaigarg@Puranjais-MacBook-Pro.local>
2023-10-22 22:43:52 -0700

    module and certificate changes

---
+++ app/components/ Certificate.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Card, Row, Col } from 'antd';
+const { Meta } = Card;
+
+const Certificate = () => {
+
+  const certificateData = [
+    {
+      title: 'Sustainable Computing',
+      description: 'CS-411 | Fall 2023',
+      color: '#FFC1A1'
+    },
+    {
+      title: 'Green Energy Solutions',
+      description: 'ES-301 | Fall 2023',
+      color: '#D9E8F5'
+    },
+    {
+      title: 'Eco-friendly Software Development',
+      description: 'CS-601-01 | Fall 2023',
+      color: '#FFC1A1'
+    },
+    {
+      title: 'Discrete Mathematics',
+      description: 'MATH 201 & 501 2023F | Fall 2023',
+      color: '#D9E8F5'
+    },
+    {
+      title: 'Compilers',
+      description: 'CS-414 | Fall 2023',
+      color: '#FFC1A1'
+    },
+    {
+      title: 'Principles of Software Development',
+      description: 'CS-602-01 | Fall 2023',
+      color: '#FFC1A1'
+    }
+  ];
+
+  return (
+    <div className='pd-1'>
+      <Row gutter={16}>
+        {certificateData.map((data, index) => (
+          <Col span={8} key={index}>
+            <Card
+              hoverable
+              style={{
+                width: 240,
+                backgroundColor: data.color,
+                borderRadius: '15px',
+                boxShadow: '0px 4px 15px rgba(0, 0, 0, 0.1)',
+                marginBottom: '20px'
+              }}
+              cover={<img alt="certificate" src="https://placeimg.com/240/160/nature" />}
+            >
+              <Meta title={data.title} description={data.description} />
+            </Card>
+          </Col>
+        ))}
+      </Row>
+      <footer style={{ textAlign: 'center', marginTop: '20px', fontSize: '12px', color: '#888' }}>
+        Green-Dons ©2023 Created by Sahil, Deven, Alexia
+      </footer>
+    </div>
+  );
+};
+
+export default Certificate;
--- app/components/Module6.js
+++ app/components/Module6.js
@@ -1,11 +1,74 @@
-import Title from "antd/es/typography/Title";
+import { Card, Image, Typography, Layout, Progress, Statistic } from 'antd';
+
+const { Title, Paragraph } = Typography;
+const { Content, Sider } = Layout;
+
+// Module 1 details (integrated directly for simplicity)
+const module1 = {
+  title: "Intro to Sustainibility",
+  introduction: "Course Introduction",
+  subtopics: [
+    "What is Sustainability?",
+    "Historical Overview of Sustainability",
+    "The Three Pillars: Economic, Environmental, and Social",
+    "Global Initiatives and Treaties",
+    "The Role of Individuals in Sustainability",
+    "Case Study: Sustainable Practices in Modern Cities"
+  ]
+};
 
 const Module6 = () => {
-    return (
-        <div>
-            <Title>Module 6</Title>
-        </div>
-    );
+  // Example values - you can replace these with actual values
+  const courseCompletion = 65; // In percent
+  const percentileCompleted = 80; // In percent
+
+  return (
+    <Layout style={{ padding: '24px', backgroundColor: '#fff' }}>
+      {/* Main content area */}
+      <Content style={{ padding: '24px', marginRight: '24px' }}>
+        <Image
+          width="100%"
+          height="80%"
+          src="./create-videos-online-courses-cover-coursifyme.jpg"
+          alt="Course Content for Module 1"
+          style={{ borderRadius: '10px', boxShadow: '0 3px 6px rgba(0,0,0,0.1)', margin: '0 auto' }}
+        />
+        <div className='text-bold text-lg'>Current course progress</div>
+        <Progress
+          style={{ marginTop: '20px' }}
+
+          percent={courseCompletion}
+          status="active"
+        />
+        <Paragraph style={{ marginTop: '10px' }}>
+          <div className='flex'>
+
+
+            <Progress type="circle" percent={percentileCompleted} />
+
+            <div className='px-6  text-lg mt-10'>Percentage of Users who have completed this module</div>
+          </div>
+          {/* <Statistic title="Percentage of Users who Have completed this module" value={percentileCompleted} /> */}
+
+          {/* {percentileCompleted}% of people have completed this course. */}
+        </Paragraph>
+      </Content>
+
+      {/* Sidebar area */}
+      <Sider width={320} style={{ backgroundColor: '#f5f5f5', padding: '24px' }}>
+        <Card bordered={false} style={{ backgroundColor: '#f5f5f5' }}>
+          <Title level={2}>{module1.title}</Title>
+          <Paragraph><strong>{module1.introduction}</strong></Paragraph>
+          <Title level={4}>Subtopics:</Title>
+          <ul style={{ listStyleType: 'disc', paddingLeft: '20px' }}>
+            {module1.subtopics.map((topic, index) => (
+              <li key={index} style={{ marginBottom: '10px' }}>{topic}</li>
+            ))}
+          </ul>
+        </Card>
+      </Sider>
+    </Layout>
+  );
 }
 
 export default Module6;
